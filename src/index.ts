f474d493a1b65332b95ef047207145410fc1feb9
Alex Uribe <repzyyog@gmail.com>
2025-03-18 10:28:28 +0100

    Create two dimnensions array 3x3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-const twoDimensionsArray = [
-  [1, 2, 3],
-  [1, 2, 3],
-  [1, 2, 3],
-];
+import { cell } from "./game_board/data.js";
+import { board } from "./game_board/data.js";
+import { Cell } from "./game_board/type.js";
+
+const gameBoard: string[][] = [];
+
+for (let positionX = 0; positionX < board.rowsTotal; positionX++) {
+  gameBoard[positionX] = [];
+  for (let positionY = 0; positionY < board.columnsTotal; positionY++) {
+    gameBoard[positionX][positionY] = "0";
+  }
+  console.log(gameBoard[positionX].join(" "));
+}
